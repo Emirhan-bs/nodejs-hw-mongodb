c0598c41d495edeb45d8b073ba2ad1ede1af42ce
Emirhan Buyuksenirli <emirhanbuyuksenirli@gmail.com>
2025-09-23 22:44:41 +0300

    fix

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
 
 export const initMongoConnection = async () => {
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}`;
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: process.env.MONGODB_DB,
-      user: process.env.MONGODB_USER,
-      pass: process.env.MONGODB_PASSWORD,
-    });
+    await mongoose.connect(uri, { dbName: process.env.MONGODB_DB });
     console.log("Mongo connection successfully established!");
   } catch (error) {
     console.error("Mongo connection failed:", error);
