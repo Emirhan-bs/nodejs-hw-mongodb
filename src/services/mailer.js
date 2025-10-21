c9e18273799ebf45b4eeea09a9ec0e5d56fab858
Emirhan Buyuksenirli <emirhanbuyuksenirli@gmail.com>
2025-10-21 11:12:02 +0300

    fix try

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -5,7 +5,7 @@ import nodemailer from "nodemailer";
 
 const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, SMTP_FROM } =
   process.env;
-
+console.log("SMTP_HOST:", SMTP_HOST);
 const transporter = nodemailer.createTransport({
   host: SMTP_HOST,
   port: Number(SMTP_PORT),
